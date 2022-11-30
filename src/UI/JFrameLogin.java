44f2c3b6ef86cbc77d58dc5ec9a038f3efeff1e5
Nam25052003 <nam028200@gmail.com>
2022-11-30 14:42:14 +0700

    reset

--- src/UI/JFrameLogin.java
+++ src/UI/JFrameLogin.java
@@ -84,11 +84,7 @@ public class JFrameLogin extends javax.swing.JFrame {
 
         txtUsername.setBackground(new java.awt.Color(37, 44, 70));
         txtUsername.setForeground(new java.awt.Color(204, 204, 204));
-<<<<<<< HEAD
         txtUsername.setText("nhien");
-=======
-        txtUsername.setText("nam");
->>>>>>> nam
         txtUsername.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(37, 44, 70)));
         txtUsername.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
@@ -380,6 +376,8 @@ public class JFrameLogin extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(JFrameLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
